8515bbedcc02bef51051b38bd8b0bf6c55c05721
MSS1410 <maarcsesa@gmail.com>
2025-04-22 18:29:43 +0200

    prueba3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
+
 const express = require('express')
-const mongoose = require('mongoose')
 const cors = require('cors')
 const connectDB = require('./config/db')
 
@@ -14,11 +14,13 @@ const routerImg = require('./routes/eventImage')
 const routerProfileImg = require('./routes/profileImg')
 
 const app = express()
-app.use(express.json())
 
-const PORT = 3051
+//middlewares
+app.use(express.json())
+app.use(cors({ origin: '*' })) // permite peticiones desde el front
+
+//bd
 connectDB()
-app.use(cors())
 
 //routers
 
@@ -29,21 +31,13 @@ app.use('/api/v1/flyer-media', routerFlyer)
 app.use('/api/v1/event-img', routerImg)
 app.use('/api/v1/users/profile', routerProfileImg)
 
-//??????
-const path = require('path')
-const FRONT_PATH = path.join(__dirname, '..', 'front')
-
-app.use(express.static(FRONT_PATH))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(FRONT_PATH, 'index.html'))
-})
-
 //500
 app.use((err, req, res, next) => {
   console.error(err)
   res.status(500).json({ message: err.message, stack: err.stack })
 })
 
+const PORT = process.env.PORT || 3051
 app.listen(PORT, () => {
   console.log(`🎧🎭 ETOMIC  en: http://localhost:${PORT}`)
 })
